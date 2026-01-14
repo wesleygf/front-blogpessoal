9c825db844ccb7e1db52b063bbf8c637368f5d37
wesleygf <wesleygfsilva@hotmail.com>
2026-01-14 16:08:02 -0300

    Adicionando o tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Home />
+      <Footer />
     </>
   );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,77 +1,28 @@
+interface homeProps {
+  title: string;
+  description: string;
+}
 function Home() {
   return (
     <>
-      <div
-        style={{
-          backgroundColor: "#312e81",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr",
-            color: "white",
-            width: "100%",
-            maxWidth: "1280px",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "1rem",
-              alignItems: "center",
-              justifyContent: "center",
-              paddingTop: "1rem",
-              paddingBottom: "1rem",
-            }}
-          >
-            <h2
-              style={{
-                fontSize: "3rem",
-                fontWeight: "bold",
-              }}
-            >
-              Seja Bem Vinde!
-            </h2>
-            <p
-              style={{
-                fontSize: "1.25rem",
-              }}
-            >
+      <div className="bg-indigo-900 flex justify-center">
+        <div className="container grid grid-cols-2 text-white">
+          <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className="text-5xl font-bold">Seja Bem Vinde!</h2>
+            <p className="text-xl">
               Expresse aqui os seus pensamentos e opiniões!
             </p>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-around",
-                gap: "1rem",
-              }}
-            >
-              <div
-                style={{
-                  borderRadius: "0.5rem",
-                  color: "white",
-                  border: "2px solid white",
-                  padding: "0.5rem 1rem",
-                }}
-              >
+            <div className="flex justify-around gap-4">
+              <div className="rounded text-white border-white border-solid border-2 py-2 px-4">
                 Nova Postagem
               </div>
             </div>
           </div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
+          <div className="flex justify-center">
             <img
               src="https://i.imgur.com/fyfri1v.png"
               alt="Imagem Pagina Home"
-              style={{ width: "66%" }}
+              className="w-2/3"
             />
           </div>
         </div>
