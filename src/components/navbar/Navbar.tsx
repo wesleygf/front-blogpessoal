b9b7cc1ea3f7ee732e4ec21a91fc51d04817220a
wesleygf <wesleygfsilva@hotmail.com>
2026-01-17 21:47:47 -0300

    Adicionando Logout

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,20 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+
+    const navigate = useNavigate();
+
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout() {
+
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
+
     return (
         <>
             <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
@@ -11,7 +25,7 @@ function Navbar() {
                         Temas
                         Cadastrar tema
                         Perfil
-                        Sair
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
                 </div>
             </div>
